7d72cbefd514efc309fea628b8464065359bf88e
Pfalconi1529 <patriciofalconi34@gmail.com>
2025-09-21 22:23:54 -0500

    inmutabilidad con copia

--- 01-creacionales/05-inmutabilidad.ts
+++ 01-creacionales/05-inmutabilidad.ts
@@ -24,7 +24,7 @@ class CodeEditorState {
         content,
         cursorPosition,
         unsaveChange
-    }: Partial<CodeEditorState>): CodeEditorState{
+    }: Partial<CodeEditorState>): CodeEditorState {
         return new CodeEditorState(
             content ?? this.content,
             cursorPosition ?? this.cursorPosition,
@@ -33,7 +33,7 @@ class CodeEditorState {
     }
 
     displayState() {
-        console.log('Estado del editor: ', COLORS.green);
+        console.log('%cEstado del editor: ', COLORS.green);
         console.log(`
             Contenido: ${this.content}
             Cursor Pos: ${this.cursorPosition}
@@ -43,7 +43,8 @@ class CodeEditorState {
 }
 
 
-class CodeEditorHistory{
+class CodeEditorHistory {
+
     private history: CodeEditorState[] = []
     private currentIndex: number = -1;
 
@@ -56,6 +57,14 @@ class CodeEditorHistory{
         this.currentIndex++ 
     }
 
+    undo(): CodeEditorState | null {
+        if(this.currentIndex > 0){
+            this.currentIndex--;
+            return this.history[this.currentIndex];
+        }
+        return null;
+    }
+
 
     redo(): CodeEditorState | null {
         if(this.currentIndex < this.history.length -1 ){
@@ -65,3 +74,37 @@ class CodeEditorHistory{
         return null;
     } 
 }
+
+
+function main(){
+    const history = new CodeEditorHistory();
+    let editorState = new CodeEditorState("console.log('Hola mundo');", 2, false);
+    history.save(editorState);
+    console.log('%cEstado inicial', COLORS.blue)
+    editorState.displayState();
+    editorState = editorState.copyWith(
+        {
+            content:"console.log('Hola mundo'); \nconsole.log('Nueva linea');",
+            cursorPosition: 3,
+            unsaveChange: true
+        }
+    );
+    history.save(editorState);
+    
+
+    console.log('\n%cDespues del primer cambio', COLORS.pink);
+    editorState.displayState();
+
+
+    console.log('\n%cDespues de mover el cursor ', COLORS.red);
+    editorState = editorState.copyWith({cursorPosition: 5});
+    history.save(editorState);
+   
+
+    console.log('\n%cDespues del undo', COLORS.brown);
+    editorState = history.undo()!;
+    history.save(editorState);
+    
+}
+
+main();
